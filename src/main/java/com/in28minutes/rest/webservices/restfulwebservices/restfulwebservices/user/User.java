12cfff66924fae2b467f42169a91a9604a32500c
Charly <charly81.zirpoli@gmail.com>
2021-12-26 14:34:42 -0300

    Jpa db

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/restfulwebservices/user/User.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/restfulwebservices/user/User.java
@@ -2,11 +2,17 @@ package com.in28minutes.rest.webservices.restfulwebservices.restfulwebservices.u
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+@Entity
 public class User {
 
+	@Id
+	@GeneratedValue
 	private Integer id;
 
 	@Size(min = 2, message = "Name should have atleast 2 characters")
@@ -14,6 +20,10 @@ public class User {
 
 	@Past
 	private Date birthDate;
+
+	public User() {
+
+	}
 
 	public User(Integer id, String name, Date birthDate) {
 		super();
