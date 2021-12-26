86f62258c540ef6e9ec19cc4b22663ac6db9e650
Charly <charly81.zirpoli@gmail.com>
2021-12-26 18:29:06 -0300

    Finish JpaResources

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
-insert into user values(1, sysdate(), 'AB');
-insert into user values(2, sysdate(), 'Jill');
-insert into user values(3, sysdate(), 'Jam');
+insert into user values(1000, sysdate(), 'AB');
+insert into user values(1001, sysdate(), 'Jill');
+insert into user values(1002, sysdate(), 'Jam');
+
+insert into post values(50, 'Mi first post', 1000);
+insert into post values(51, 'Mi second post',1000);
